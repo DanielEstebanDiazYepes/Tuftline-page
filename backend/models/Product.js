5ae6d93d0046e2f2fcaaf0f4d8c36e693e941a8b
Daniel Diaz <diazyepez7@gmail.com>
2025-04-13 17:48:10 -0500

    ahora se pueden agregar productos a la base de dataos y salen en la pagina

---
+++ backend/models/Product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const storeConnection = require('../config/store_db');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  imageUrl: String
+});
+
+module.exports = storeConnection.model('Product', productSchema);
